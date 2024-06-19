d5da48945f0e5ce58d90ceb7bf5d0e670927f7f2
Gavin.Mao <kekemao00@gmail.com>
2024-06-19 20:32:35 +0800

    :bug: fix: Optimized DataStore

--- app/src/main/kotlin/me/kekemao/protonkit/MainActivity.kt
+++ app/src/main/kotlin/me/kekemao/protonkit/MainActivity.kt
@@ -12,12 +12,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.runBlocking
 import me.kekemao.base_compose.modifier.debouncedClickable
+import me.kekemao.keyvaluestore.DataStoreManager
+import me.kekemao.keyvaluestore.DataStoreManager.Companion.SP_KEY_MESSAGE
 import me.kekemao.protonkit.ui.theme.ProtonKitTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +33,7 @@ class MainActivity : ComponentActivity() {
             ProtonKitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Android", modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -37,28 +41,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val num1 = remember { mutableIntStateOf(0) }
     val num = remember { mutableIntStateOf(0) }
+
+    var dataStore: DataStoreManager? = null
+    val msg = remember { mutableStateOf("NULL") }
+
+    LaunchedEffect(Unit) {
+        dataStore = DataStoreManager(context)
+        dataStore?.save(SP_KEY_MESSAGE, "Hello, DataStoreManager${System.currentTimeMillis()}")
+        msg.value = runBlocking { dataStore?.retrieve(SP_KEY_MESSAGE, "NULL")?:"NULL" }
+    }
+
     Column {
 
         Text(
-            text = "Hello $name!",
-            modifier = modifier
+            text = "${msg.value}!", modifier = modifier
         )
 
-        Button(
-            onClick = { ++num.intValue },
-            Modifier.debouncedClickable {
-                ++num1.intValue
-                Toast.makeText(
-                    context,
-                    "Button clicked",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }) {
+        Button(onClick = { ++num.intValue }, Modifier.debouncedClickable {
+            ++num1.intValue
+            Toast.makeText(
+                context, "Button clicked", Toast.LENGTH_SHORT
+            ).show()
+        }) {
             Text("Click Me ${num1.intValue}, ${num.intValue}")
         }
 
--- key-value-store/src/main/kotlin/me/kekemao/keyvaluestore/DataStoreManager.kt
+++ key-value-store/src/main/kotlin/me/kekemao/keyvaluestore/DataStoreManager.kt
@@ -2,11 +2,10 @@ package me.kekemao.keyvaluestore
 
 import android.content.Context
 import android.util.Log
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.doublePreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
@@ -15,11 +14,11 @@ import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import me.kekemao.base.BaseApplication
 
 /**
  * Data store manager。
@@ -30,249 +29,92 @@ import me.kekemao.base.BaseApplication
  * @init 初始化：本工具类无需初始化，导入依赖即用： implementation("androidx.datastore:datastore-preferences:1.0.0")
  *
  */
-object DataStoreManager {
+data class DataStoreManager(
+    private val context: Context, private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+
+    companion object {
+        /**
+         * 消息 KEY
+         */
+        const val SP_KEY_MESSAGE: String = "SP_KEY_MESSAGE"
+
+    }
 
     private val TAG: String = DataStoreManager::class.java.name
 
     /**
      * 默认 DataStore 名称
      */
-    private const val DEFAULT_PREFERENCES_NAME = "default_preferences_name"
+    private val DEFAULT_PREFERENCES_NAME = "default_preferences_name"
 
     /**
      * 获取 DataStore 对象
      */
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-        name = DEFAULT_PREFERENCES_NAME
-    )
+    private val Context.dataStore by preferencesDataStore(name = DEFAULT_PREFERENCES_NAME)
 
     /**
      * 获取 DataStore 对象
      */
-    private val dataStore = BaseApplication.appContext.dataStore
+    private val dataStore get() = context.dataStore
+
 
     /**
-     * IO 线程调度器
-     */
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
-
-    /**
-     * 保存 String 类型的数据
+     * 保存数据
      *
-     * @param key
-     * @param value
+     * @param key 保存的 key  @see [SP_KEY_MESSAGE]
+     * @param value 保存的数据 (String, Set<String>, Int, Long, Float, Double, Boolean)
      */
-    private suspend fun saveString(key: String, value: String) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[stringPreferencesKey(key)] = value }
-        }
-
-    /**
-     * 保存 String Set 类型的数据
-     *
-     * @param key
-     * @param value
-     */
-    private suspend fun saveStringSet(key: String, value: Set<String>) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[stringSetPreferencesKey(key)] = value }
-        }
-
-    /**
-     * 保存 Int 类型的数据
-     *
-     * @param key
-     * @param value
-     */
-    private suspend fun saveInt(key: String, value: Int) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[intPreferencesKey(key)] = value }
-        }
-
-    /**
-     * 保存 Long 类型的数据
-     *
-     * @param key
-     * @param value
-     */
-    private suspend fun saveLong(key: String, value: Long) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[longPreferencesKey(key)] = value }
-        }
-
-    /**
-     * 保存 Float 类型的数据
-     *
-     * @param key
-     * @param value
-     */
-    private suspend fun saveFloat(key: String, value: Float) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[floatPreferencesKey(key)] = value }
-        }
-
-    /**
-     * 保存 Double 类型的数据
-     *
-     * @param key
-     * @param value
-     */
-    private suspend fun saveDouble(key: String, value: Double) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[doublePreferencesKey(key)] = value }
-        }
-
-    /**
-     * 保存 Boolean 类型的数据
-     *
-     * @param key
-     * @param value
-     */
-    private suspend fun saveBoolean(key: String, value: Boolean) =
-        withContext(ioDispatcher) {
-            dataStore.edit { it[booleanPreferencesKey(key)] = value }
-        }
-
-    /**
-     * 获取 String 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveString(key: String, default: String? = null): String? =
-        runBlocking {
-            dataStore.data.map { it[stringPreferencesKey(key)] ?: default }.first()
-        }
-
-    /**
-     * 获取 String Set 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveStringSet(key: String, default: Set<String>? = null): Set<String>? =
-        runBlocking {
-            dataStore.data.map { it[stringSetPreferencesKey(key)] ?: default }.first()
-        }
-
-    /**
-     * 获取 Int 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveInt(key: String, default: Int? = null): Int? =
-        runBlocking {
-            dataStore.data.map { it[intPreferencesKey(key)] ?: default }.first()
-        }
-
-    /**
-     * 获取 Long 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveLong(key: String, default: Long? = null): Long? =
-        runBlocking {
-            dataStore.data.map { it[longPreferencesKey(key)] ?: default }.first()
-        }
-
-    /**
-     * 获取 Float 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveFloat(key: String, default: Float? = null): Float? =
-        runBlocking {
-            dataStore.data.map { it[floatPreferencesKey(key)] ?: default }.first()
-        }
-
-    /**
-     * 获取 Double 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveDouble(key: String, default: Double? = null): Double? =
-        runBlocking {
-            dataStore.data.map { it[doublePreferencesKey(key)] ?: default }.first()
-        }
-
-    /**
-     * 获取 Boolean 类型的数据
-     *
-     * @param key
-     * @param default
-     * @return
-     */
-    private fun retrieveBoolean(key: String, default: Boolean? = null): Boolean? =
-        runBlocking {
-            dataStore.data.map {
-                it[booleanPreferencesKey(key)] ?: default
-            }.first()
-        }
-
-    /**
-     * 保存任意类型的数据
-     *
-     * @param T
-     * @param key
-     * @param value
-     */
-    @Suppress("UNCHECKED_CAST")
     suspend fun <T> save(key: String, value: T) {
-        Log.d(TAG, "save: $key=$value\"")
-        when (value) {
-            is String -> saveString(key, value)
-            is Set<*> -> saveStringSet(key, value as Set<String>)
-            is Int -> saveInt(key, value)
-            is Long -> saveLong(key, value)
-            is Float -> saveFloat(key, value)
-            is Double -> saveDouble(key, value)
-            is Boolean -> saveBoolean(key, value)
-            else -> throw IllegalArgumentException("This type cannot be saved to the Data Store")
+        Log.v(TAG, "save: Storing locally: $key=$value")
+        withContext(ioDispatcher) {
+            dataStore.edit { preferences ->
+                when (value) {
+                    is String -> preferences[stringPreferencesKey(key)] = value
+                    is Set<*> -> preferences[stringSetPreferencesKey(key)] = value as Set<String>
+                    is Int -> preferences[intPreferencesKey(key)] = value
+                    is Long -> preferences[longPreferencesKey(key)] = value
+                    is Float -> preferences[floatPreferencesKey(key)] = value
+                    is Double -> preferences[doublePreferencesKey(key)] = value
+                    is Boolean -> preferences[booleanPreferencesKey(key)] = value
+                    else -> throw IllegalArgumentException("Unsupported type")
+                }
+            }
         }
     }
 
     /**
-     * 获取任意类型的数据
+     * 读取数据
      *
-     * @param T
-     * @param key
-     * @param default
-     * @return
+     * @param key 读取的 key  @see [SP_KEY_MESSAGE]
+     * @param defaultValue 默认值 (String, Set<String>, Int, Long, Float, Double, Boolean)
      */
     @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
-    fun <T> retrieve(key: String, default: T): T {
-
-        val result = when (default) {
-            is String -> retrieveString(key, default)
-            is Set<*> -> retrieveStringSet(key, default as Set<String>)
-            is Int -> retrieveInt(key, default)
-            is Long -> retrieveLong(key, default)
-            is Float -> retrieveFloat(key, default)
-            is Double -> retrieveDouble(key, default)
-            is Boolean -> retrieveBoolean(key, default)
-            else -> throw IllegalArgumentException("This type cannot be saved to the Data Store")
-        } as T
-
-        Log.d(TAG, "retrieve: $key=$result")
-
-        return result
+    fun <T> retrieve(key: String, defaultValue: T): T {
+        Log.v(TAG, "retrieve: Reading local cache: $key")
+        return runBlocking {
+            dataStore.data.catch {
+                Log.w(TAG, it)
+                emit(emptyPreferences())
+            }.map { preferences ->
+                when (defaultValue) {
+                    is String -> preferences[stringPreferencesKey(key)] ?: defaultValue
+                    is Set<*> -> preferences[stringSetPreferencesKey(key)] ?: defaultValue
+                    is Int -> preferences[intPreferencesKey(key)] ?: defaultValue
+                    is Long -> preferences[longPreferencesKey(key)] ?: defaultValue
+                    is Float -> preferences[floatPreferencesKey(key)] ?: defaultValue
+                    is Double -> preferences[doublePreferencesKey(key)] ?: defaultValue
+                    is Boolean -> preferences[booleanPreferencesKey(key)] ?: defaultValue
+                    else -> throw IllegalArgumentException("Unsupported type")
+                } as T
+            }.first()
+        }
     }
 
     /**
      * 清空所有数据
      */
-    suspend fun clearData() =
-        withContext(ioDispatcher) {
-            dataStore.edit { it.clear() }
-        }
+    suspend fun clearData() = withContext(ioDispatcher) {
+        dataStore.edit { it.clear() }
+    }
 }
